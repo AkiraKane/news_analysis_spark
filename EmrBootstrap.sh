714c1c7d4c96e0a49e1e4e83ca282c9b2a60d061
Warren Hamilton Ronsiek <warrenronsiek@gmail.com>
2017-04-14 18:29:35 -0700

    added something approaching sensible to emrbootstrap.sh

--- EmrBootstrap.sh
+++ EmrBootstrap.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-sudo wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
-tar xvf apache-maven-3.5.0-bin.tar.gz
-sudo mv apache-maven-3.5.0  /usr/local/apache-maven
-export M2_HOME=/usr/local/apache-maven
-export M2=$M2_HOME/bin
-export PATH=$M2:$PATH
-source ~/.bashrc
-curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
-sudo yum install sbt
+aws s3 cp s3://warren-datasets/spark-corenlp-0.2.0-s_2.10.jar ./spark-corenlp-0.2.0-s_2.10.jar
+aws s3 cp s3://warren-datasets/stanford-english-kbp-corenlp-2016-10-31-models.jar ./stanford-english-kbp-corenlp-2016-10-31-models.jar
+aws s3 cp s3://warren-datasets/stanford-english-corenlp-2016-10-31-models.jar ./stanford-english-corenlp-2016-10-31-models.jar
+aws s3 cp s3://warren-datasets/stanford-corenlp-full-2016-10-31.zip ./stanford-corenlp-full-2016-10-31.zip
+tar -xvf stanford-corenlp-full-2016-10-31.zip
 
-sudo yum update -y
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get install sbt
+sudo mv ./spark-corenlp-0.2.0-s_2.10.jar /usr/lib/spark/jars/spark-corenlp-0.2.0-s_2.10.jar
+sudo mv ./stanford-english-kbp-corenlp-2016-10-31-models.jar /usr/lib/spark/jars/stanford-english-kbp-corenlp-2016-10-31-models.jar
+sudo mv ./stanford-english-corenlp-2016-10-31-models.jar /usr/lib/spark/jars/stanford-english-corenlp-2016-10-31-models.jar
